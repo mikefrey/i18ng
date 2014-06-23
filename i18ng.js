efe4222dcdff16aa0952b098b590bf0e79c85609
Mike Frey <mike.frey@sportngin.com>
2014-06-23 15:57:19 -0500

    better options and html support

--- i18ng.js
+++ i18ng.js
@@ -34,28 +34,80 @@ angular.module('i18ng')
     }
   }])
 
-  .directive('i18n', ['i18ng', function(i18ng) {
+  .directive('i18n', ['i18ng', '$parse', function(i18ng, $parse) {
     'use strict'
 
-    function translate(scope, element) {
-      var val = i18ng.t(scope.i18n, scope.i18opts)
-      element.text(val)
+    var attrRx = /^i18n(.+?)(Opts)?$/
+
+    function translateAll(scope, element, translations) {
+      angular.forEach(translations, function(val, attr) {
+        translate(scope, element, translations, attr)
+      })
+    }
+
+    function translate(scope, element, translations, attr) {
+      var getKey = translations[attr].getKey || angular.noop
+      var getOpts = translations[attr].getOpts || angular.noop
+      var key = getKey(scope) || ''
+      var val = i18ng.t(key, getOpts(scope))
+      if (attr == '_html')
+        element.html(val)
+      else if (attr == '_text')
+        element.text(val)
+      else
+        element.attr(attr, val)
     }
 
     return {
       restrict: 'A',
-      scope: {
-        i18n: '=',
-        i18opts: '='
-      },
-      link: function(scope, element, attr) {
-        var t = translate.bind(this, scope, element)
+      link: function(scope, element, attrs) {
 
-        scope.$watch('i18n', t)
-        scope.$watch('i18opts', t)
-        scope.$on('i18ngInitComplete', t)
+        var translations = {}
+        var t = translate.bind(null, scope, element, translations)
+        var ignore = ['opts', 'html']
 
-        t()
+        angular.forEach(attrs, function(val, key) {
+          var match = key.match(attrRx)
+          if (match && match[1] && !~ignore.indexOf(match[1].toLowerCase())) {
+            var attr = match[1].toLowerCase()
+            var prop = !!match[2] ? 'getOpts' : 'getKey'
+            var watch = !!match[2] ? '$watchCollection' : '$watch'
+
+            if (!translations[attr]) translations[attr] = {}
+            translations[attr][prop] = $parse(val)
+
+            scope[watch](function() {
+              return translations[attr][prop](scope)
+            }, function() {
+              t(attr)
+            })
+          }
+        })
+
+        if (attrs.i18n) {
+          var attr = 'i18ngHtml' in attrs ? '_html' : '_text'
+          var hasOpts = !!attrs.i18nOpts
+          translations[attr] = {
+            getKey: $parse(attrs.i18n),
+            getOpts: hasOpts ? $parse(attrs.i18nOpts) : null
+          }
+
+          scope.$watch(function() {
+            return translations[attr].getKey(scope)
+          }, function() { t(attr) })
+
+          if (hasOpts) {
+            scope.$watchCollection(function() {
+              return translations[attr].getOpts(scope)
+            }, function() { t(attr) })
+          }
+        }
+
+        scope.$on('i18ngInitComplete', function() {
+          translateAll(scope, element, translations)
+        })
+
+        translateAll(scope, element, translations)
       }
     }
   }])
